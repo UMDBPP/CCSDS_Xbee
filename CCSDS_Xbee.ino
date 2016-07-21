4eef158570a2ac6ac7f064b096e273cdc4b9ef57
Steve <steve.lentine@gmail.com>
2016-07-21 08:16:11 -0400

    Added arduino sketch so that compiling of the library can be tested

---
+++ CCSDS_Xbee.ino
@@ -0,0 +1,14 @@
+#include<Wire.h>
+#include<XBee.h>
+#include"ccsds_xbee.h"
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
