42d0797365a19fd7c277480395a0bd8a1a82a59c
Steve <ascended121@gmail.com>
2016-12-03 12:27:15 -0500

    removed unneeded buffers from example implementation, fixed bugs with using wrong buffer, added NAME command

--- examples/Z_example_implementation/Z_example_implementation.ino
+++ examples/Z_example_implementation/Z_example_implementation.ino
@@ -32,6 +32,10 @@
 #include <EEPROM.h>
 
 //// Enumerations
+// Name
+#define PAYLOAD_NAME "Test" 
+// Must be no longer than 8 characters
+
 // APIDS
 #define CMD_APID 200
 #define HK_APID 210
@@ -42,6 +46,8 @@
 #define TIME_APID 216
 #define FILEINFO_APID 217
 #define FILEPART_APID 218
+#define NAME_APID 219
+
 
 // FcnCodes
 #define NOOP_FCNCODE 0
@@ -54,6 +60,7 @@
 #define REQTIME_FCNCODE 16
 #define REQFILEINFO_FCNCODE 17
 #define REQFILEPART_FCNCODE 18
+#define REQNAME_FCNCODE 19
 #define RESETCTR_FCNCODE 20
 #define SETTIME_FCNCODE 21
 #define REBOOT_FCNCODE 99
@@ -553,6 +560,58 @@ void command_response(uint8_t CmdData[], uint8_t data_len, struct IMUData_s IMUD
       CmdExeCtr++;
       break;
     }
+    // REQ_Name Cmd
+    case REQNAME_FCNCODE:
+    {
+      /* 
+       * This command requests that a packet containing the payload's
+       * name be sent to a specific xbee. The format of the command is:
+       *   CCSDS Command Header (8 bytes)
+       *   Xbee address (uint8_t)
+       * There is one parameter associated with this command, the address
+       * of the xbee to send the Name message to. The format of the Name message
+       * which is sent out is:
+       *   CCSDS Telemetry Header (12 bytes)
+       *   Payload Name (string, 8 bytes)
+       */ 
+      debug_serial.print("Received Name Cmd to addr ");
+
+      // define variables to process the command
+      uint8_t NamedestAddr = 0;
+      uint16_t pktLength = 0;
+      uint8_t payloadLength = 0;
+      int success_flg = 0;
+      
+      // define buffer to create the response in
+      uint8_t Name_Payload_Buff[PKT_MAX_LEN];
+      
+      // extract the desintation address from the command
+      extractFromTlm(NamedestAddr, CmdData, 8);
+      debug_serial.println(NamedestAddr);
+
+      // Use sprintf to pad/trim the string if the payload name isn't
+      // exactly 8 characters
+      char payloadname[8]; 
+      sprintf(payloadname,"%8.8s",PAYLOAD_NAME);
+
+      // print the name to debug
+      debug_serial.println(payloadname);
+      
+      // add the information to the buffer
+      payloadLength = addStrToTlm(payloadname, Name_Payload_Buff, payloadLength);
+
+      // send the telemetry message by adding the buffer to the header
+      success_flg = ccsds_xbee.sendTlmMsg(NamedestAddr, NAME_APID, Name_Payload_Buff, payloadLength);
+            
+      if(success_flg > 0){
+        // increment the cmd executed counter
+        CmdExeCtr++;
+      }
+      else{
+        CmdRejCtr++;
+      }
+      break;
+    } 
     // REQ_HK Cmd
     case REQHK_FCNCODE:
     {
@@ -578,10 +637,10 @@ void command_response(uint8_t CmdData[], uint8_t data_len, struct IMUData_s IMUD
       uint8_t HKdestAddr = 0;
       uint16_t pktLength = 0;
       uint8_t payloadLength = 0;
+      int success_flg = 0;
       
       // define buffers to create the response in
       uint8_t HK_Payload_Buff[PKT_MAX_LEN];
-      uint8_t HK_Pkt_Buff[PKT_MAX_LEN];
       
       // extract the desintation address from the command
       extractFromTlm(HKdestAddr, CmdData, 8);
@@ -590,16 +649,16 @@ void command_response(uint8_t CmdData[], uint8_t data_len, struct IMUData_s IMUD
       // add the information to the buffer
       payloadLength = create_HK_payload(HK_Payload_Buff);
 
-      // create the telemetry message by adding the buffer to the header
-      pktLength = ccsds_xbee.createTlmMsg(HK_Pkt_Buff, HK_APID, HK_Payload_Buff, payloadLength);
-      
-      if(pktLength > 0){
-        // send the data
-        ccsds_xbee.sendRawData(HKdestAddr, HK_Pkt_Buff, pktLength); 
+      // send the telemetry message by adding the buffer to the header
+      success_flg = ccsds_xbee.sendTlmMsg(HKdestAddr, HK_APID, HK_Payload_Buff, payloadLength);
+            
+      if(success_flg > 0){
+        // increment the cmd executed counter
+        CmdExeCtr++;
       }
-
-      // increment the cmd executed counter
-      CmdExeCtr++;
+      else{
+        CmdRejCtr++;
+      }
       break;
     } 
     // ResetCtr
@@ -651,7 +710,6 @@ void command_response(uint8_t CmdData[], uint8_t data_len, struct IMUData_s IMUD
       
       // define buffers to create the response in
       uint8_t ENV_Payload_Buff[PKT_MAX_LEN];
-      uint8_t ENV_Pkt_Buff[PKT_MAX_LEN];
       
       // extract the desired xbee address from the packet
       extractFromTlm(EnvDestAddr, CmdData, 8);
@@ -697,7 +755,6 @@ void command_response(uint8_t CmdData[], uint8_t data_len, struct IMUData_s IMUD
       
       // define buffers to create the response in
       uint8_t PWR_Payload_Buff[PKT_MAX_LEN];
-      uint8_t PWR_Pkt_Buff[PKT_MAX_LEN];
       
       // extract the desired xbee address from the packet
       extractFromTlm(PwrDestAddr, CmdData, 8);
@@ -707,7 +764,7 @@ void command_response(uint8_t CmdData[], uint8_t data_len, struct IMUData_s IMUD
       payloadLength = create_PWR_payload(PWR_Payload_Buff, PWRData);
       
       // send the telemetry message by adding the buffer to the header
-      success_flg = ccsds_xbee.sendTlmMsg(PwrDestAddr, PWR_APID, PWR_Pkt_Buff, payloadLength);
+      success_flg = ccsds_xbee.sendTlmMsg(PwrDestAddr, PWR_APID, PWR_Payload_Buff, payloadLength);
       
       if(success_flg > 0){
         // increment the cmd executed counter
@@ -754,7 +811,6 @@ void command_response(uint8_t CmdData[], uint8_t data_len, struct IMUData_s IMUD
       
       // define buffers to create the response in
       uint8_t IMU_Payload_Buff[PKT_MAX_LEN];
-      uint8_t IMU_Pkt_Buff[PKT_MAX_LEN];
       
       // extract the desired xbee address from the packet
       extractFromTlm(ImuDestAddr, CmdData, 8);
@@ -764,7 +820,7 @@ void command_response(uint8_t CmdData[], uint8_t data_len, struct IMUData_s IMUD
       payloadLength = create_IMU_payload(IMU_Payload_Buff, IMUData);
       
       // send the telemetry message by adding the buffer to the header
-      success_flg = ccsds_xbee.sendTlmMsg(ImuDestAddr, IMU_APID, IMU_Pkt_Buff, payloadLength);
+      success_flg = ccsds_xbee.sendTlmMsg(ImuDestAddr, IMU_APID, IMU_Payload_Buff, payloadLength);
       
       if(success_flg > 0){
         // increment the cmd executed counter
@@ -806,7 +862,6 @@ void command_response(uint8_t CmdData[], uint8_t data_len, struct IMUData_s IMUD
       
       // define buffers to create the response in
       uint8_t Init_Payload_Buff[PKT_MAX_LEN];
-      uint8_t Init_Pkt_Buff[PKT_MAX_LEN];
       
       // extract the desired xbee address from the packet
       extractFromTlm(InitDestAddr, CmdData, 8);
@@ -877,7 +932,6 @@ void command_response(uint8_t CmdData[], uint8_t data_len, struct IMUData_s IMUD
       
       // define buffers to create the response in
       uint8_t Time_Payload_Buff[PKT_MAX_LEN];
-      uint8_t Time_Pkt_Buff[PKT_MAX_LEN];
       
       // extract the desired xbee address from the packet
       extractFromTlm(TimeDestAddr, CmdData, 8);
@@ -887,7 +941,7 @@ void command_response(uint8_t CmdData[], uint8_t data_len, struct IMUData_s IMUD
       payloadLength = addIntToTlm(rtc.now().unixtime(), Time_Payload_Buff, payloadLength); // Add time to message [uint32_t]
       
       // send the telemetry message by adding the buffer to the header
-      success_flg = ccsds_xbee.sendTlmMsg(TimeDestAddr, TIME_APID, Time_Pkt_Buff, payloadLength);
+      success_flg = ccsds_xbee.sendTlmMsg(TimeDestAddr, TIME_APID, Time_Payload_Buff, payloadLength);
       
       if(success_flg > 0){
         // increment the cmd executed counter
@@ -926,7 +980,6 @@ void command_response(uint8_t CmdData[], uint8_t data_len, struct IMUData_s IMUD
       
       // define buffers to create the response in
       uint8_t FileInfo_Payload_Buff[PKT_MAX_LEN];
-      uint8_t FileInfo_Pkt_Buff[PKT_MAX_LEN];
       
       // extract the desired xbee address from the packet
       pkt_pos = extractFromTlm(FileInfoDestAddr, CmdData, 8);
@@ -944,7 +997,7 @@ void command_response(uint8_t CmdData[], uint8_t data_len, struct IMUData_s IMUD
       if(payloadLength > 0){
         
         // send the telemetry message by adding the buffer to the header
-        success_flg = ccsds_xbee.sendTlmMsg(FileInfoDestAddr, FILEINFO_APID, FileInfo_Pkt_Buff, payloadLength);
+        success_flg = ccsds_xbee.sendTlmMsg(FileInfoDestAddr, FILEINFO_APID, FileInfo_Payload_Buff, payloadLength);
         
         if(success_flg > 0){
           // increment the cmd executed counter
@@ -995,7 +1048,6 @@ void command_response(uint8_t CmdData[], uint8_t data_len, struct IMUData_s IMUD
       
       // define buffers to create the response in
       uint8_t FilePart_Payload_Buff[PKT_MAX_LEN];
-      uint8_t FilePart_Pkt_Buff[PKT_MAX_LEN];
         
       // extract the desired xbee address from the packet
       pkt_pos = extractFromTlm(FilePartDestAddr, CmdData, 8);
@@ -1033,7 +1085,7 @@ void command_response(uint8_t CmdData[], uint8_t data_len, struct IMUData_s IMUD
       if(payloadLength > 0){
           
         // send the telemetry message by adding the buffer to the header
-        success_flg = ccsds_xbee.sendTlmMsg(FilePartDestAddr, FILEPART_APID, FilePart_Pkt_Buff, payloadLength);
+        success_flg = ccsds_xbee.sendTlmMsg(FilePartDestAddr, FILEPART_APID, FilePart_Payload_Buff, payloadLength);
         
         if(success_flg > 0){
           // increment the cmd executed counter
@@ -1416,7 +1468,7 @@ uint16_t create_FileInfo_payload(uint8_t Payload_Buff[], uint8_t file_idx){
 
       // Add counter values to the pkt
       char filename[13]; // max filename is 8 characters + 1 period + 3 letter extention + 1 null term
-      sprintf(filename,"%12s",entry.name());
+      sprintf(filename,"%12.12s",entry.name());
 
       for(int i = 0; i < 13; i++){
         debug_serial.print(filename[i]);
